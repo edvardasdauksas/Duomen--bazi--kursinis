39c7f700b2164a5d6f07a5c83d3a8470f43d11af
Mantas <mojavosas@gmail.com>
2011-10-27 20:08:58 +0300

    Sukelti failai check_login.php, index.html, script.js, styles.css, su.jpg

---
+++ check_login.php
@@ -0,0 +1,24 @@
+﻿<?php
+	
+	$ak=$_POST["ak"];
+	$id=$_POST["id"];
+	
+	
+$conn = oci_connect('mantas', 'fluffy', 'localhost/XE');
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message' + 'what the hell'], ENT_QUOTES), E_USER_ERROR);
+}
+
+$stid = oci_parse($conn, "SELECT * FROM RINKEJAI WHERE AK='$ak' AND PAZYMEJIMO_NR='$id'");
+oci_execute($stid);
+
+if ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+	echo 1;
+}
+else {
+	echo 0;
+}
+	oci_free_statement($stid);
+	oci_close($conn);
+?>
---
+++ script.js
@@ -0,0 +1,28 @@
+﻿function check_login() {
+	var req;
+	var stuff = "ak=" + document.login.asmk.value + "&id=" + document.login.idnum.value;
+	req=new XMLHttpRequest();
+	req.onreadystatechange=function() {
+		if (req.readyState==4 && req.status==200) {
+			whatnow=1*req.responseText;
+			if (whatnow === 1) {
+				login_go();
+			} else {
+				login_no();
+			}
+		}
+	}
+	req.open("POST","check_login.php",true);
+	req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	req.send(stuff);
+	//document.write('sin');
+}
+
+function login_go() {
+	document.write("YOU HAVE LOGGED IN, SIR");
+}	
+
+function login_no() {
+	document.getElementById("fail").innerHTML="GO AWAY";
+}
+
